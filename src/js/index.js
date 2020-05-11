6a581b54bb46e7b99eaa03597e15ad62c10dc3ca
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2020-05-12 03:02:17 +0800

    creation of Search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,38 @@
 // Global app controller
+// import axios from 'axios';
+
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
+
+const state = {};
+
+// TEST
+window.state = state;
+
+const controlSearch = async() => {
+
+  // get query from user
+  const query = searchView.getInput();
+
+
+  if (query) {
+    // create new search object and add to state
+    state.search = new Search(query);
+
+    // prepare the UI
+    searchView.clearInput();
+
+    try {
+
+    await state.search.getResults();
+    } catch(error) {
+      alert(error);
+    }
+  }
+};
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
---
+++ src/js/models/Search.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    try {
+      const res = await axios(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${this.query}`);
+      this.result = res.data.drinks;
+      console.log(this.result);
+    } catch(error) {
+      alert(error);
+    }
+  }
+}
