db133d2342cb4de525d79388098819cefb5eda8b
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2020-05-14 01:24:38 +0800

    more scraping for Cocktail model

--- src/js/models/Cocktail.js
+++ src/js/models/Cocktail.js
@@ -14,9 +14,30 @@ export default class Cocktail {
       console.log(result);
       this.title = result.strDrink;
       this.image = result.strDrinkThumb;
-      // this.ingredients = ;
-      // this.measures = ;
+      this.ingredients = [];
       this.description = result.strInstructions;
+
+      for (let [key, value] of Object.entries(result)) {
+        if (key.includes('Ingredient')) {
+          if (value) {
+            this.ingredients.push(value);
+          }
+        }
+      }
+
+      const measures = [];
+      for (let [key, value] of Object.entries(result)) {
+        if (key.includes('Measure')) {
+          if (value) {
+            measures.push(value);
+          }
+        }
+      }
+
+      for (let i = 0; i < this.ingredients.length; i++) {
+        this.ingredients[i] += ',' + measures[i];
+      }
+
 
     } catch(error) {
       console.log(error);
