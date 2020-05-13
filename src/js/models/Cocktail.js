23871a33dd8d5903f061518c468e9c5e8471a96c
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2020-05-14 01:31:38 +0800

    added comments on Cocktail model

--- src/js/models/Cocktail.js
+++ src/js/models/Cocktail.js
@@ -8,15 +8,14 @@ export default class Cocktail {
   async getRecipe() {
     try {
 
-      const res = await axios(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${this.id}`)
-      console.log(res);
+      const res = await axios(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${this.id}`);
       const result = res.data.drinks[0];
-      console.log(result);
       this.title = result.strDrink;
       this.image = result.strDrinkThumb;
       this.ingredients = [];
       this.description = result.strInstructions;
 
+      // get all ingredients into an array
       for (let [key, value] of Object.entries(result)) {
         if (key.includes('Ingredient')) {
           if (value) {
@@ -25,6 +24,7 @@ export default class Cocktail {
         }
       }
 
+      // get all the measures, and add it to each ingredient
       const measures = [];
       for (let [key, value] of Object.entries(result)) {
         if (key.includes('Measure')) {
